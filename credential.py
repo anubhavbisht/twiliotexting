7a4d0cb60fedf08003581b012bad0935d1b2e278
anubhav <anubhavbisht98@gmail.com>
2020-03-11 13:04:15 +0530

    texting project

---
+++ credential.py
@@ -0,0 +1,7 @@
+##in this file you enter your credentials which can be used for sending
+##messages
+
+my_sid=''##here enter your unique sid number from twilio site
+my_auth_token=''##here enter your unique authorised token from twilio site
+my_cell=''##here enter the reciever mobile number which has to be verified by your twilio account beforehand
+my_twilio=''##here enter your unique twilio number which acts as sender
