44930e4926e910e8c20057306085d6fe6e68e42d
Anubhav Bisht <40894333+anubhavbisht@users.noreply.github.com>
2020-03-19 23:03:01 +0530

    Update send.py

--- send.py
+++ send.py
@@ -4,4 +4,4 @@ from credential import my_sid, my_auth_token, my_cell, my_twilio
 client=Client(my_sid,my_auth_token)
 my_msg="hello buddy!!"##enter your message
 message=client.messages.create(to=my_cell,from_=my_twilio,body=my_msg)
-##then simpy run file(send.py) and wait for the message in your mobile
+##then simply run file(send.py) and wait for the message in your mobile
